63e6ed037f9be8454035a0c56409927f293ba05e
Khulan-25 <khulan225@gmail.com>
2024-03-07 16:53:41 +0900

    app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from './src/Button';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "./src/components/Button";
+import Header from "./src/components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button type={'Primary'} text={'MIU BUTTON'}></Button>
+      <Header
+        buttontext={["About", "Academics", "Admission", "Life at MIU", "LEI"]}
+      ></Header>
     </View>
   );
 }
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
