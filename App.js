3834f5c5caec9d87f86c932a0f0c349ee2c8d2a6
Khulan-Ts <khulan225@gmail.com>
2024-03-12 22:11:40 +0800

    feat: images and logos

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Button from "./src/components/Button";
-import Header from "./src/components/Header";
+import CardButton from "./src/components/cardButtons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header
-        type={"Primary"}
-        buttontext={["about us", "academics", "admissions", "LEI"]}
-      ></Header>
+      <CardButton image={require("./assets/image.png")} text={'Computer Science'}></CardButton>
     </View>
   );
 }
