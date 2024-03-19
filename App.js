218d8dd8f24d6016a0de494d16494535e63bc417
anarrdn <garnate2k@gmail.com>
2024-03-19 23:41:38 +0800

    title is not working, lot of questions will be asked tmrw hhe

--- App.js
+++ App.js
@@ -1,33 +1,59 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Button from "./src/components/Button";
+import React, { useState } from "react";
+import { StyleSheet, Text, Image, Animated, View } from "react-native";
 import Header from "./src/components/Header";
+import Title from "./src/components/Titles";
+import Button from "./src/components/Button";
 
 export default function App() {
+  const [scrollY] = useState(new Animated.Value(0));
+
+  const headerBackgroundColor = scrollY.interpolate({
+    inputRange: [0, 40],
+    outputRange: ["rgba(61, 37, 98, 1)", "rgba(61, 37, 98, 0.5)"],
+    extrapolate: "clamp",
+  });
+
   return (
-    <View style={styles.container}>
-      <Header
+    <Animated.ScrollView
+      style={styles.container}
+      stickyHeaderIndices={[0]}
+      onScroll={Animated.event(
+        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+        { useNativeDriver: false }
+      )}
+    >
+        <Header
           type={"Primary"}
           buttontext={["about us", "academics", "admissions", "LEI"]}
-          style ={{backgroundColor: "#3d2562" }}
+          style ={{backgroundColor: headerBackgroundColor }}
         />
-        <Button type='Secondary' text="Undergraduate"></Button>
-        <Button type='Secondary' text="Graduate"></Button>
-        <Button type='Secondary' text="Exchange Student Program" numberOfLines={2}></Button>
-        <Button type='Third' text="Sport Hall"></Button>
-        <Button type='Fourth' text="Housing"></Button>
-        <Button type='Fifth' text="For More Information"></Button>
-        <Button type='Sixth' text="Dormitory Fee"></Button>
-        <Button type='Apply' text="Application Fee"></Button>
-    </View>
+      <View style={{marginLeft: 203, width:136, height:35}}>
+       
+        <View style={{marginTop: 159,borderBottomWidth: 1, width: 210, paddingBottom: 14,paddingLeft:14}}>
+        <Title type="Subtitle" text={'Undergraduate'} style={{color: '#62253E'}}></Title>
+        </View>
+        <Button type="Secondary" text={"Undergraduate"}></Button>
+        <Button type="Secondary" text={"Graduate"}></Button>
+        <Button type="Secondary" text={"2+2 program"}></Button>
+        <Button type="Secondary" text={"Exchange student Program"}></Button>
+        <Button type="Secondary" text={"Vision Trip"}></Button>
+        <Button type="Secondary" text={"Academic Calendar"}></Button>
+      </View>
+    </Animated.ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "gray",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  header: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 100,
+    elevation: 3, // Required for Android
+    // Other header styles such as height, padding, etc.
   },
 });
