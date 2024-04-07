d597fc37e5ef2d93ed52fa9fd1a0ce25c6bd5d50
ZakharSm <113816034+ZakharSm@users.noreply.github.com>
2024-04-07 16:51:38 +0800

    2p2 updated

--- Twoplus2.js
+++ Twoplus2.js
@@ -13,22 +13,25 @@ const Twoplus2=({navigation})=>{
       style={styles.container}
     >
         <View style={{marginLeft: 100, width:136, height:35}}>
-          <View style={{marginTop: 159,borderBottomWidth: 1, width: 210, paddingBottom: 14,paddingLeft:14}}>
-          <FONT text={'Undergraduate'} style={{color: '#62253E'}}></FONT>
+          <View style={{marginTop: 159,borderBottomWidth: 1, width: 210, paddingBottom: 14}}>
+            <FONT style={{color: 'black', textAlign:'left', fontSize: 28}}>Academics</FONT>
           </View>
-          <Button type="Secondary" text={"Undergraduate"}></Button>
-          <Button type="Secondary" text={"Graduate"}></Button>
-          <Button type="Secondary" text={"2+2 program"}></Button>
-          <Button type="Secondary" text={"Exchange student Program"}></Button>
-          <Button type="Secondary" text={"Vision Trip"}></Button>
-          <Button type="Secondary" text={"Academic Calendar"}></Button>
-        </View>
+          <View style={{marginLeft: 25, marginTop: 25}}>
+            <Button type="Secondary" text={"Undergraduate"}></Button>
+            <Button type="Secondary" text={"Graduate"}></Button>
+            <Button type="Secondary" text={"2+2 program"}></Button>
+            <Button type="Secondary" text={"Exchange student Program"}></Button>
+            <Button type="Secondary" text={"Vision Trip"}></Button>
+            <Button type="Secondary" text={"Academic Calendar"}></Button>
+          </View>
+
+      </View>
         <View style={styles.imageContainer}>
           <Image
-            source={require("./assets/2p2.png")}
+            source={require("./assets/ualbany.png")}
             style={styles.image}
           />
-          <View style={{backgroundColor: '#EDF0FF', marginTop:40, width:'100%'}}>
+          <View style={[styles.backgroundContainer, { marginTop:40, width:'70%'}]}>
             <FONT style={{color: '#000000', textAlign:'left', fontSize: 23}}>INTRODUCTION</FONT>
           </View>
 
@@ -60,18 +63,17 @@ const Twoplus2=({navigation})=>{
             </View>
 
           </View>
-        </View>
-        <View style={styles.infoContainer}>
+          <View style={styles.infoContainer}>
           <View style={styles.infoSection}>
-            <View style={{backgroundColor: '#EDF0FF', marginTop:40}}>
+            <View style={[styles.backgroundContainer2, {backgroundColor: '#EDF0FF', marginTop:40}]}>
               <FONT style={{color: '#000000',  fontSize: 23, textAlign:'center'}}>QUALIFICATIONS</FONT>
             </View>
-            <FONT style={styles.text}>• Requirements: Undergraduate students, who completed 2 years at MIU</FONT>
+            <FONT style={styles.text}>• Requirements: Undergraduate students,{'\n'}   who completed 2 years at MIU</FONT>
             <FONT style={styles.text}>• GPA: 3.4/4.0 or higher</FONT>
             <FONT style={styles.text}>• English Test: TOEFL IBT 70 or IELTS 6.0 or above</FONT>
           </View>
-          <View style={styles.infoSection}>
-            <View style={{backgroundColor: '#EDF0FF', marginTop:40}}>
+          <View style={styles.infoSection2}>
+            <View style={[styles.backgroundContainer2, {backgroundColor: '#EDF0FF', marginTop:40, marginBottom:20}]}>
               <FONT style={{color: '#000000', fontSize: 23, textAlign:'center'}}>EXPENSES</FONT>
             </View>
             <FONT style={styles.text}>• 1 year Tuition: USD 28,350</FONT>
@@ -79,6 +81,15 @@ const Twoplus2=({navigation})=>{
             <FONT style={[styles.text, {fontWeight:'bold'}]}>• Total: USD 42,132</FONT>
           </View>
         </View>
+        </View>
+        <View style={styles.verticalImage}>
+        <Image
+          source={require('./assets/verticalimg.png')}
+          style={styles.verticalImageImg}
+          resizeMode="contain" // Adjust the resizeMode as needed
+        />
+      </View>
+        
 
 
 
@@ -95,6 +106,7 @@ export default Twoplus2
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor:'##f9f8f5',
   },
   header: {
     position: "absolute",
@@ -107,14 +119,15 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     width:'75%',
-    marginTop:'10%',
+    marginTop: 140,
     marginLeft:'30%',
     flex: 1,
   },
   
   image: {
-    width: 500,
-    height: 300,
+    width: '60%',
+    height:'60%',
+    // height: 400,
   },
   title: {
     textAlign: 'left',
@@ -160,15 +173,48 @@ const styles = StyleSheet.create({
   },
   infoContainer: {
     flexDirection: 'row',
-    marginTop: 50,
+    marginTop: 70,
     marginBottom:'10%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    alignItems: 'left',
   },
   infoSection: {
     flexDirection: 'column',
-    width: '45%', 
-    alignItems:'left',
+    alignItems:'flex-start',
+    marginRight:159,
+  },
+  infoSection2: {
+    flexDirection: 'column',
+    alignItems:'flex-start',
+  },
+  backgroundContainer: {
+    overflow: 'hidden', // Ensure the gradient doesn't overflow its container
+    alignSelf: 'flex-start', // Align the container to the start of its parent
+    backgroundColor: 'transparent', // Ensure the text is visible
+    // Add gradient background
+    backgroundImage: 'linear-gradient(to right, #C7C4F2 20%, transparent)', // Adjust gradient direction and colors as needed
+  },
+  backgroundContainer2: {
+    paddingRight: 150,
+    baoverflow: 'hidden', // Ensure the gradient doesn't overflow its container
+    alignSelf: 'flex-start', // Align the container to the start of its parent
+    backgroundColor: 'transparent', // Ensure the text is visible
+    // Add gradient background
+    backgroundImage: 'linear-gradient(to right, #C7C4F2, transparent)', // Adjust gradient direction and colors as needed
+  },
+  verticalImage: {
+    position: 'absolute',
+    top: 0,
+    right: 50,
+    bottom: 0,
+    width: 100, // Adjust the width as needed
+    justifyContent: 'center', // Align the image vertically
+    alignItems: 'center', // Center the image horizontally
+  },
+  verticalImageImg: {
+    // height: '100%',
+    width: '120%',
+    aspectRatio: 1, // Maintain aspect ratio
   },
 
 });
