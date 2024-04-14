53166456629b19c71995f06ef46156b7a4b3ece9
Duluu00 <duluu_2001@yahoo.com>
2024-04-15 00:25:13 +0800

    Graduate and Dorm(just beginning)

---
+++ Dorm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, Image, ScrollView, View } from "react-native";
+import CardButton from "./src/components/cardButtons";
+import FONT from "./src/components/Titles";
+import Button from "./src/components/Button";
+
+const Dorm=({navigation})=>{
+
+    return(
+        <ScrollView style={styles.container}>
+            <View style={{ flexDirection: 'row', marginTop: 159}}>
+                <View>
+                    <View style={{marginLeft: 160, borderBottomWidth:1, width: 210, paddingBottom: 4, paddingLeft: 14}}>
+                        <FONT type="Regular" style={{color: ''}}>Dorm</FONT>
+                    </View >
+                    <View style={{marginTop: 20, marginLeft: 160}}>
+                        <View style={{borderBottomWidth: 1, width: 144}}>
+                            <Button type="Secondary" text={"Housing"} ></Button>
+                        </View>
+                        <View style={{borderBottomWidth: 1, width: 144}}>
+                            <Button type="Secondary" text={"Fee Information"} ></Button>
+                        </View>
+                        <View style={{borderBottomWidth: 1, width: 144}}>
+                            <Button type="Secondary" text={"Application Form"} ></Button>
+                        </View>
+                    </View>
+                </View>
+            </View>
+
+        </ScrollView>
+    )
+}
--- Graduate.js
+++ Graduate.js
@@ -13,14 +13,26 @@ const Graduate=({navigation})=>{
           <View style={{marginLeft: 160, borderBottomWidth:1, width: 210, paddingBottom: 4, paddingLeft: 14}}>
             <FONT type="Regular" style={{color: ''}}>Academics</FONT>
           </View >
-          <View style={{marginLeft: 157, marginTop: 33, color: ''}}>
-            <Button style={{color: ''}} type="Secondary" text={'Undergraduate'}>
-            </Button>
-            <Button style={{color: ''}} type="Secondary" text={'Graduate'}>
-            </Button> 
-          </View>
+          <View style={{marginTop: 20, marginLeft: 160}}>
+            <View style={{borderBottomWidth: 1, width: 144}}>
+            <Button type="Secondary" text={"Undergraduate"} ></Button>
+            </View>
+            <View style={{borderBottomWidth: 1, width: 144}}>
+            <Button type="Secondary" text={"Graduate"} ></Button>
+            </View>
+            <View style={{borderBottomWidth: 1, width: 144}}>
+            <Button type="Secondary" text={"2+2 program"} ></Button>
+            </View>
+            <Button type="Secondary" numberOfLines={2} text={"Exchange student Program"} ></Button>
+            <View style={{borderBottomWidth: 1, width: 144}}>
+            </View>
+            <View style={{borderBottomWidth: 1, width: 144}}>
+            <Button type="Secondary" text={"Vision Trip"} ></Button>
+            </View>
+            <Button type="Secondary" text={"Academic Calendar"}></Button>
+            </View>
         </View>
-        <View style={{width: 1025, height: 151, marginLeft: 49}}>
+        <View style={{width: 1025, height: 151, marginLeft: 49, marginBottom: 132}}>
           <FONT type={"Subtitle2"} style={{marginTop: 48, color: ''}}>Master Program</FONT>
           <FONT type={"Body"}style={{marginTop: 30, color: ''}}>MIU challenges students to explore innovative intellectual interests through scholarly and personal competency programs. Emphasizing on professionalism, MIU students learn to make new connections in a synergistic environment that multiplies the use of resources such as time, energy, and creativity. </FONT>
           <View style={{width: 1025, height: 29, marginTop: 30, backgroundColor: '#EDF0FF'}}>
@@ -49,7 +61,7 @@ const Graduate=({navigation})=>{
           <View style={{width: 1025, height: 29, marginTop: 30, backgroundColor: '#EDF0FF'}}>
             <FONT type={"Subtitle2"} style={{color: ''}}>Programs</FONT>
           </View>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{flexDirection: 'row', }}>
             <Button style={{color: ''}} type="Secondary" text={'Master In Foreign Language Education (FLE)'}>
             </Button>
           </View>
