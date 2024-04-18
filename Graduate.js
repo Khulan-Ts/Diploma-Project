5e8a72457d1787b73499068af922fb94e12a79ea
Khulan-Ts <khulan225@gmail.com>
2024-04-18 09:45:57 +0800

    add: Graduate.js from Duluu branch

---
+++ Graduate.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { StyleSheet, useWindowDimensions, Image, ScrollView, View } from "react-native";
+import FONT from "./src/components/Titles";
+import Button from "./src/components/Button";
+
+const Graduate= ({ navigation }) => {
+  const { width, height } = useWindowDimensions();
+  return (
+    <ScrollView
+      style={styles.container}
+    >
+      <View style={{flexDirection: "row"}}>
+        <View style={{marginLeft: width * 0.1, width: width * 0.2}}>
+            <View style={{marginTop: height * 0.3, borderBottomWidth: 1, width: width * 0.15, paddingBottom: 14, paddingLeft:14}}>
+                <FONT type="Regular" style={{color: '#000000'}}>Academics</FONT>
+            </View>
+            <View style={{marginTop: height * 0.02}}>
+                <View style={{borderBottomWidth: 1, width: width * 0.11}}>
+                    <Button type="Secondary" text={"Undergraduate"} ></Button>
+                </View>
+                <View style={{borderBottomWidth: 1, width: width * 0.11}}>
+                    <Button type="Secondary" text={"Graduate"} ></Button>
+                </View>
+                    <View style={{borderBottomWidth: 1, width: width * 0.11}}>
+                <Button type="Secondary" text={"2+2 program"} ></Button>
+                </View>
+                <View style={{borderBottomWidth: 1, width: width * 0.11}}>
+                
+                <Button type="Secondary" text={"Exchange student Program"} numberOfLines={2} style={{width: width * 0.12}}></Button>
+                
+                </View>
+                <View style={{borderBottomWidth: 1, width: width * 0.11}}>
+                    <Button type="Secondary" text={"Vision Trip"} ></Button>
+                </View>
+                <Button type="Secondary" text={"Academic Calendar"}></Button>
+            </View>
+        </View>
+        <View style={{width: width * 0.5, marginTop: height * 0.3, marginBottom: height * 0.15}}>
+          <FONT type={"Subtitle2"} style={{marginTop: 48, color: ''}}>Master Program</FONT>
+          <FONT type={"Body"}style={{marginTop: 30, color: ''}}>MIU challenges students to explore innovative intellectual interests through scholarly and personal competency programs. Emphasizing on professionalism, MIU students learn to make new connections in a synergistic environment that multiplies the use of resources such as time, energy, and creativity. </FONT>
+          <View style={{width: 1025, height: 29, marginTop: 30, backgroundColor: '#EDF0FF'}}>
+            <FONT type={"Subtitle2"} style={{color: ''}}>School of Management (SoM)</FONT>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Button style={{color: ''}} type="Secondary" text={'Master in Business Administration (MBA)'}>
+            </Button>
+            <Button style={{color: '', marginLeft: 55}} type="Secondary" text={'Master’s Start-Up Business Administration'}>
+            </Button>
+          </View>
+          <View style={{width: 1025, height: 29, marginTop: 30, backgroundColor: '#EDF0FF'}}>
+            <FONT type={"Subtitle2"} style={{color: ''}}>School of Computing Science (SoCS)</FONT>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Button style={{color: ''}} type="Secondary" text={'Master of Sofware Engineering (MSE)'}>
+            </Button>
+          </View>
+          <View style={{width: 1025, height: 29, marginTop: 30, backgroundColor: '#EDF0FF'}}>
+            <FONT type={"Subtitle2"} style={{color: ''}}>School of International Studies and Communication (SoISC)</FONT>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Button style={{color: ''}} type="Secondary" text={'School of International Studies and Communication (SoISC)'}>
+            </Button>
+          </View>
+          <View style={{width: 1025, height: 29, marginTop: 30, backgroundColor: '#EDF0FF'}}>
+            <FONT type={"Subtitle2"} style={{color: ''}}>Programs</FONT>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Button style={{color: ''}} type="Secondary" text={'Master In Foreign Language Education (FLE)'}>
+            </Button>
+          </View>
+        </View>
+        <View style={{width: width * 0.1, marginTop: height * 0.16, marginBottom: height * 0.3, marginLeft: 'auto'}}>
+          <Image source={require('./assets/images/cal-1.png')} style={{width: 'auto', height: width * 0.5}} resizeMode="contain"/>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+export default Graduate
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+});
