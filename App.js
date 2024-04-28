8017df38fe799fff32f2924638ebfb3885499859
Duluu00 <duluu_2001@yahoo.com>
2024-04-28 21:32:53 +0800

    Overview

--- App.js
+++ App.js
@@ -12,6 +12,14 @@ import Exchange from './screens/Academic/Exchange';
 import AcademicCal from './screens/Academic/AcademicCalendar';
 import Dorm from './screens/Dorm/Dorm_AF'
 import Overview from './screens/Admissions/Overview';
+import Lei_1 from './screens/LEI/Lei_1'
+import Lei_2 from './screens/LEI/Lei_2'
+import Lei_3 from './screens/LEI/Lei_3'
+import Lei_4 from './screens/LEI/Lei_4'
+import Lei_5 from './screens/LEI/Lei_5'
+import AboutUs from './screens/About us/AboutUs'
+import OurPeople from './screens/About us/OurPeople'
+
 
 const Stack = createStackNavigator();
 
@@ -40,7 +48,7 @@ const App = () => {
         <Stack.Screen name="2 + 2 program" component={Twoplus2} />
         <Stack.Screen name="Exchange student Program" component={Exchange} />
         <Stack.Screen name="Academic Calendar" component={AcademicCal} />
-        <Stack.Screen name="Another" component={AnotherScreen} />
+        <Stack.Screen name="Another" component={Overview} />
         <Stack.Screen name="Dorm" component={Dorm} />
         <Stack.Screen name="English Foundation Course" component={Lei_1} />
         <Stack.Screen name="English Evening Course" component={Lei_2} />
@@ -50,7 +58,6 @@ const App = () => {
         <Stack.Screen name="About us" component={AboutUs} />
         <Stack.Screen name="Our people" component={OurPeople} /> */}
         <Stack.Screen name="Over View" component={Overview} />
-
       </Stack.Navigator>
     </NavigationContainer>
   );
