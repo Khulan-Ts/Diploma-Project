a13069e168a042576fc350d12febc8a1cf473112
Khulan-Ts <khulan225@gmail.com>
2024-05-01 10:12:13 +0800

    hoverbutton fix

--- Home.js
+++ Home.js
@@ -32,9 +32,9 @@ const HomeScreen= ({ navigation }) =>{
       
       <View style={{alignItems: 'center', marginTop: '-10%', zIndex: 3}}>
         <View style={{flexDirection: 'row'}}>
-          <HoverButton icon={require("./assets/icons/teachers.png")} text={"99+ Teachers"} nummber={'99+'}></HoverButton>
-          <HoverButton icon={require("./assets/icons/research.png")} text={"12+ Research"} nummber={'12+'} style={{marginLeft: width * 0.03}}></HoverButton>
-          <HoverButton icon={require("./assets/icons/department.png")} text={"15+ Departments"} nummber={'15+'} style={{marginLeft: width * 0.03}}></HoverButton>
+          <HoverButton icon={require("./assets/icons/teachers.png")} text={"International Faculty"} nummber={'77%'}></HoverButton>
+          <HoverButton icon={require("./assets/icons/research.png")} text={"International Students"} nummber={'12+'} style={{marginLeft: width * 0.03}}></HoverButton>
+          <HoverButton icon={require("./assets/icons/department.png")} text={"Taught in English"} nummber={'15+'} style={{marginLeft: width * 0.03}}></HoverButton>
         </View> 
       </View>
       <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: width * 0.05}}>
